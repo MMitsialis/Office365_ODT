4374b3c6c45f830640f142396f47d22ea754daf6
Marc Mitsialis <MMitsialis@users.noreply.github.com>
2017-04-07 12:28:07 +0200

    Added Change-OfficeChannel* to switch channels of an existing installation. Added Install-Office365ProPlus_VisioPro-*-CBB_CDN-Update_CDN* series of XMLs and CMDs
    Removed superfluous and superseded files.
     Download.office365ProPlus.16.0.7571.2109.x64.xml
     Download.office365ProPlus.16.0.7571.2109.xml
     Download.office365ProPlus.CBB.16.0.6965.2140.xml
     Download.office365ProPlus.x64.CB.xml
     Download.office365ProPlus.x64.CBB.xml

---
+++ Change-OfficeChannelFirstReleaseDeferred.ps1
@@ -0,0 +1,2 @@
+Change-OfficeChannel\Change-OfficeChannel.ps1  -Channel FirstReleaseDeferred  -Verbose
+
---
+++ Install-Office365ProPlus_VisioPro-x86-CBB_CDN-Update_CDN.cmd
@@ -0,0 +1,1 @@
+setup.exe  /Configure Install-Office365ProPlus_VisioPro-x86-CBB_CDN-Update_CDN
