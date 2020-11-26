0900ba98822d595b7481bc6a5704c1e998e06700
Marc Mitsialis <Marc.Mitsialis@GMail.Com>
2020-11-26 18:03:30 +0200

    Added Script to sync to MTN (CMP-01)

---
+++ Sync2MTN.ps1
@@ -0,0 +1,26 @@
+& net use O: "\\ZA-FE-CMP-01.mtn.com\H$\Software Repository$" /User:mtngroup\adm234093
+
+Set-Location -Path "C:\Office365_ODT\"
+$WorkDir = Get-Location
+$Days = 21
+$ScripRunStart = Get-Date -Format "yyyyMMdd_HHmm"
+Start-Transcript -Path "$WorkDir\Sync2MTN-$ScripRunStart.Transcript"
+
+$SourceList = Get-ChildItem  -path ".\Office\Data\" |  Where-Object {$_.LastWriteTime -ge (Get-Date).AddDays(-($Days)) }
+
+$DataLocation = "$WorkDir\Office\Data\"
+$DestinationLocation = "O:\Microsoft\Office Applications\Office365\Office\Data\"
+Set-Location $DataLocation
+
+foreach ($Source in $SourceList) {
+	If ( $Source.PSIsContainer -eq $true )  {
+		& robocopy $Source.Name $DestinationLocation$Source /ETA /S /TBD /w:5 /R:30
+		}
+	If ($Source.Extension -eq ".cab") {
+		Copy-Item -Path $Source.Name -Destination $DestinationLocation  -Force -Verbose
+	}
+
+
+}
+Set-Location $WorkDir
+Stop-Transcript
